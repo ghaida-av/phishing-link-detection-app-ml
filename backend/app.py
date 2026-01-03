39f2b420d78468d0bd33a12c033ed34329cfb679
Ayda Elsanosi <ghaidaasan97@gmail.com>
2026-01-03 17:30:37 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,14 @@
 from flask import Flask, request, jsonify
-
 app = Flask(__name__)
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the URL from the Android app
     data = request.get_json()
     url = data.get("url", "").lower()
-    
-    
-    # We check for common phishing signs
     is_phishing = False
-    if len(url) > 50: is_phishing = True        # Real phishing links are often very long
-    if "@" in url: is_phishing = True           # Phishers use @ to hide true domains
-    if "https" not in url: is_phishing = True    # Most phishing sites lack SSL certificates
-
+    if len(url) > 50: is_phishing = True       
+    if "@" in url: is_phishing = True         
+    if "https" not in url: is_phishing = True    
     result = "PHISHING" if is_phishing else "SAFE"
     return jsonify({"verdict": result})
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
